58a3c510b31ee6ad8767cf5c65b4ad86949f4e3e
atsta <ath.ctam@gmail.com>
2020-11-16 00:27:47 +0200

    add everything

---
+++ src/main/java/com/dbproject/chicagoincidents/ChicagoIncidentsApplication.java
@@ -0,0 +1,13 @@
+package com.dbproject.chicagoincidents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChicagoIncidentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChicagoIncidentsApplication.class, args);
+    }
+
+}
