b499b4748a90d24165c6669428ec8604bc343889
atsta <atstamatiou@gmail.com>
2020-11-27 14:06:26 +0200

    added repository and controller

---
+++ src/main/java/com/dbproject/chicagoincidents/repository/RequestRepository.java
@@ -0,0 +1,10 @@
+package com.dbproject.chicagoincidents.repository;
+
+import com.dbproject.chicagoincidents.domain.Request;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RequestRepository extends CrudRepository<Request, Integer>{
+}
