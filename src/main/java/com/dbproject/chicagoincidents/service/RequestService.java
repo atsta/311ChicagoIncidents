b1cdf966a59cbb98c91d26947b097c677045d82c
atsta <atstamatiou@gmail.com>
2020-11-30 13:56:10 +0200

    added request service

---
+++ src/main/java/com/dbproject/chicagoincidents/service/RequestService.java
@@ -0,0 +1,29 @@
+package com.dbproject.chicagoincidents.service;
+
+import com.dbproject.chicagoincidents.domain.Request;
+import com.dbproject.chicagoincidents.repository.RequestRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class RequestService {
+
+    @Autowired
+    RequestRepository requestRepository;
+
+    public Request addRequest(Request request) {
+        requestRepository.save(request);
+        return request;
+    }
+
+    public Iterable<Request> getRequests(){
+        return requestRepository.findAll();
+    }
+
+    public Optional<Request> getRequestRepository(Long id) {
+        return requestRepository.findById(id);
+    }
+}
